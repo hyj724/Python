2f209c9d2a20ed55a7d2bb2df83ec6f6f6189a5c
Chloe <69901687+hyj724@users.noreply.github.com>
2021-02-06 16:06:37 +0800

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -21,7 +21,7 @@ def translate(w):
         similar_w = get_close_matches(w,data.keys())[0]
         # ask user if he wants the definition of the similar word
         yn = input(f"Do you mean {similar_w} instead? Enter Y if yes, enter N if no: ")
-        yn = yn.strip()
+        yn = yn.upper().strip()
         while yn != "Y" and yn != "N":
             yn = input("Please enter a valid letter(Y/N): ")
         if yn == "Y":
@@ -43,3 +43,63 @@ while True:
             print(line)
     else:
         print(output)
+
+
+        
+# Method 2: mysql.connetor
+# Unlike previous example, words are not case-sensitive in the database.
+# So, there's no need to lower/upper/title user's input. We just jump to look for similar words.
+
+import mysql.connector
+from difflib import get_close_matches
+
+con = mysql.connector.connect(
+    user = "ardit700_student",
+    password = "ardit700_student",
+    host = "108.167.140.122",
+    database = "ardit700_pm1database"
+)
+
+cursor = con.cursor()
+
+def translate(w):
+    # The table we use is called "Dictionary".The results is: [(expression,definition),...]
+    query = cursor.execute(f"SELECT * FROM Dictionary WHERE Expression = '{w}'")
+    results = cursor.fetchall()
+
+    if results:
+        # store only the definition elements
+        results = [result[1] for result in results]
+        return results
+    else:
+        # get all expressions
+        query = cursor.execute("SELECT Expression FROM Dictionary")
+        total_ws = cursor.fetchall()
+        # the formation is [(definition,),...], exert the first element
+        total_ws = [total_w[0] for total_w in total_ws]
+
+        if len(get_close_matches(w,total_ws)) > 0:
+            similar_w = get_close_matches(w,total_ws)[0]
+            yn = input(f"Do you mean {similar_w} instead? Enter Y if yes, enter N if no: ")
+            yn = yn.upper().strip()
+            while yn != "Y" and yn != "N":
+                yn = input("Pleae input a valid letter: ")
+                yn = yn.upper().strip()
+            if yn == "Y":
+                return translate(similar_w)
+            else:
+                return "The word doesn't exist."
+
+while True:
+    word = input("Please enter a word(press '\exit' to exit): ")
+    if word == '\exit':
+        break
+
+    output = translate(word)
+    if isinstance(output,list):
+        for line in output:
+            print(line)
+    else:
+        print(output)
+
+
